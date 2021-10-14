766a082918dd929f9ff6593c7d7c14ad3fc340c8
ThandiM81 <thandiwe.mtetwa@googlemail.com>
2021-10-14 16:52:25 +0100

    Added basic search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
-      <p>Thandi's dictionary app</p>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <p>Thandi's dictionary app</p>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
